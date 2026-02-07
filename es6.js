286b613cfd9ae5ca9257c2816f1cf829bbeae0a7
Afroza-Nipa13 <afrozaakter228@gmail.com>
2026-02-07 23:02:55 +0600

    first commit

---
+++ es6.js
@@ -0,0 +1,20 @@
+const tax = 5000;
+let eta = 5;
+eta = 2
+
+
+const student = { name : 'nayeem', marks: 50}
+const friends = [ 'nipa', 'liza', 'khadiza', 'masoud', 'sarwar'
+]
+
+// default parameter
+
+function add(num1, num2 = 0){
+
+}
+
+// template string
+
+const dynamicText = `My Tax: ${tax} and marks ${student.marks + 2} has friend : ${friends[3]}`;
+
+console.log(dynamicText)
